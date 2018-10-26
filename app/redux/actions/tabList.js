b7b3e4d0cdc7ff74b2315d1b2465fa63093c262c
dupi <duxianwei520@126.com>
2018-10-26 16:30:06 +0800

    dupi update readme and so on

--- app/redux/actions/tabList.js
+++ app/redux/actions/tabList.js
@@ -3,4 +3,4 @@ import { createAction } from 'redux-actions'
 export const requestTabList = createAction('request tab list')
 export const updateTabList = createAction('update tab list')
 export const updateTabChecked = createAction('update tab checked')
-export const deleteTabFromList = createAction('delete tab from list');
+export const deleteTabFromList = createAction('delete tab from list')
